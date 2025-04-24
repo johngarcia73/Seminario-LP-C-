825fbcdfd7462e6834bf687388ac72c6b0e0e875
Victor H. <127906875+Victor003h@users.noreply.github.com>
2025-04-23 23:03:11 -0400

    Add files via upload

--- vector class/vec.cpp
+++ vector class/vec.cpp
@@ -35,6 +35,15 @@ public:
             values[i]=other.values[i];
     }
 
+    //one value
+    Vec(int value){
+        for (int i = 0; i < N; i++)
+        {
+            values[i]=value;
+        }
+    }
+    
+
     int size(){
         return N;
     }
@@ -75,7 +84,6 @@ std::ostream& operator<<(std::ostream& os, const Vec<T, N>& v) {
     return os;
 }
 
-
 // Vector of size 2
 template<typename T>
 class Vec<T,2>{
@@ -111,6 +119,15 @@ public:
             values[i]=other.values[i];
     }
 
+    // one Value
+    Vec(int value){
+        for (int i = 0; i < 2; i++)
+        {
+            values[i]=value;
+        }
+    }
+
+
     int size(){
         return 2;
     }
@@ -139,17 +156,7 @@ public:
 
 };
 
-template <typename T, int>
-std::ostream& operator<<(std::ostream& os, const Vec<T, 2>& v) {
-    os << "(";
-    for (int i = 0; i < 2; ++i) {
-        os << v[i];
-        if (i != 2 - 1)
-            os << ", ";
-    }
-    os << ")";
-    return os;
-}
+
 
 // Vector of size 3
 template<typename T>
@@ -186,6 +193,14 @@ public:
             values[i]=other.values[i];
     }
 
+    //one value
+    Vec(int value){
+        for (int i = 0; i < 3; i++)
+        {
+            values[i]=value;
+        }
+    }
+
     int size(){
         return 3;
     }
@@ -214,15 +229,63 @@ public:
 
 };
 
-template <typename T, int>
-std::ostream& operator<<(std::ostream& os, const Vec<T, 3>& v) {
-    os << "(";
-    for (int i = 0; i < 3; ++i) {
-        os << v[i];
-        if (i != 3 - 1)
-            os << ", ";
-    }
-    os << ")";
-    return os;
-}
 
+
+int main() {
+    // -------------------------------
+    // Caso general: vec<double, 4>
+    // -------------------------------
+    std::cout << "Probando Vec<int, 4> (caso general)" << std::endl;
+    Vec<double, 4> v4;
+
+    // Inicialización mediante operator[]
+    for (int i = 0; i < 4; ++i) {
+        v4[i] = i; 
+    }
+    std::cout << v4;
+   
+    std::cout << "\n\n";
+
+    // -------------------------------
+    // Caso especializado: Vec<int, 2> (2D)
+    // -------------------------------
+    std::cout << "Probando Vec<int, 2> (especialización 2D)" << std::endl;
+    // Inicialización mediante constructor a partir de arreglo
+    int arr2[2] = {10, 20};
+    Vec<int, 2> v2(arr2);
+    std::cout << "v2 (acceso por campos): x = " << v2.x << ", y = " << v2.y << std::endl;
+
+    // Modificar elementos utilizando operator[]
+    v2[0] = 30;
+    v2[1] = 40;
+    std::cout << "v2 (tras modificar con operator[]): x = " << v2.x << ", y = " << v2.y << "\n\n";
+    
+    // -------------------------------
+    // Caso especializado: vec<float, 3> (3D)
+    // -------------------------------
+    std::cout << "Probando vec<float, 3> (especialización 3D)" << std::endl;
+    // Construcción por defecto y asignación a través de miembros
+    Vec<float, 3> v3;
+    v3.x = 1.0f;
+    v3.y = 2.0f;
+    v3.z = 3.0f;
+    std::cout << "v3 (acceso por miembros): (" << v3.x << ", " << v3.y << ", " << v3.z << ")" << std::endl;
+
+    // Modificar elementos utilizando operator[]
+    v3[0] = 4.0f;
+    v3[1] = 5.0f;
+    v3[2] = 6.0f;
+    std::cout << "v3 (tras modificar con operator[]): (" 
+              << v3.x << ", " << v3.y << ", " << v3.z << ")" << std::endl;
+
+    
+    
+    // Aquí para vec<float,3> se espera que sizeof(v3) sea igual a 3 * sizeof(float)
+    if(sizeof(v3) == 3 * sizeof(float)){
+        std::cout<<"Se verifica que sizeof(v3) sea igual a 3* sizeof(float)";
+    }
+    
+    
+
+    return 0;
+}
